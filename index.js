dc28c7704ff1fd5997ed024acd07cb9b58599851
melkilic <melike180400@gmail.com>
2020-05-04 08:47:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+ string.slice( 1 )
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string){
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let stringToCapitalize = string.charAt(0).toUpperCase() + string.slice(1)
+    let array = stringToCapitalize.split(" ")
+    return array.map(string => {
+      if (exceptions.includes(string)) {
+        return string
+      } else {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      }
+    }).join(" ")
+
+
+
+
+  }
+  }
